b04c570ab8268aa53a4aac91fddb04e5e77d6225
Rodrigo Lourenço <rodrigofplourenco2002@gmail.com>
2023-02-03 00:05:05 +0000

    Aula 3

---
+++ 3_C_Comments.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+// Quando temos 2 barras no inicio de uma linha, significa que é um comentário, ou seja
+// O compilador vai ignorar todos os comentários, faça uso deles para explicar peças de codigo complexas
+
+/*
+ Use a sintaxe de /* para ter um comentário
+ de várias linhas!
+ e o // para comentários de 1 linha
+*/
+
+int main()
+{
+  // Printa olá mundo na tela, este é um exemplo de um comentário
+  printf("Olá, mundo!\n"); // tambem podemos ter comentários depois de sentenças
+
+  return 0;
+}
